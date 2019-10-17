1d51fd4671c4e1cb220acb2182866d61d290fffa
christianmsc <christian1601y@hotmail.com>
2019-10-17 18:01:50 -0300

    Início dos ajustes na página Default

--- VestidosAdmin/Default.aspx.cs
+++ VestidosAdmin/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cDados;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,13 @@ namespace VestidosAdmin
 {
     public partial class Default : System.Web.UI.Page
     {
+        cEmpresa objEmpresa = new cEmpresa();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            List<cEmpresa> empresasComPlanos = objEmpresa.Listar("idPlano IS NOT NULL ORDER BY id DESC");
+            grdEmpresasPlanos.DataSource = empresasComPlanos;
+            DataBind();
         }
     }
 }
